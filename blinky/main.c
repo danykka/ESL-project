ad98a3214b0c165fe86bfaaf105e5a713cb8e8f4
Daniil Mozgaryov <86235265+danykka@users.noreply.github.com>
2022-11-02 18:56:12 +0300

    Update main.c

--- blinky/main.c
+++ blinky/main.c
@@ -8,13 +8,18 @@ const int SECOND_DIGIT = 5;
 const int THIRD_DIGIT = 8;
 const int FOURTH_DIGIT = 9;
 
-void blink_n_times(const int n)
+const int FIRST_LED_NUM = 0;
+const int SECOND_LED_NUM = 1;
+const int THIRD_LED_NUM = 2;
+const int FOURTH_LED_NUM = 3;
+
+void blink_n_times(const int n, const int led_number)
 {
     for (int i = 0; i < n; i++)
        {
-           bsp_board_led_invert(0);
+           bsp_board_led_invert(led_number);
            nrf_delay_ms(300);
-           bsp_board_led_invert(0);
+           bsp_board_led_invert(led_number);
            nrf_delay_ms(300);
        }
        nrf_delay_ms(1000);
@@ -26,9 +31,9 @@ int main(void)
 
     while (true)
     {
-        blink_n_times(FIRST_DIGIT);
-        blink_n_times(SECOND_DIGIT);
-        blink_n_times(THIRD_DIGIT);
-        blink_n_times(FOURTH_DIGIT);
+        blink_n_times(FIRST_DIGIT, FIRST_LED_NUM);
+        blink_n_times(SECOND_DIGIT, SECOND_LED_NUM);
+        blink_n_times(THIRD_DIGIT, THIRD_LED_NUM);
+        blink_n_times(FOURTH_DIGIT, FOURTH_LED_NUM);
     }
 }
